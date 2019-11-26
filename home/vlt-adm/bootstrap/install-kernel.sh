6024d82388ba8cf99710713ffe7aeff9924f030d
Jérémie Jourdin <jeremie.jourdin@advens.fr>
2019-11-26 11:02:47 +0100

    Add support for FreeBSD Kernel Tools

--- home/vlt-adm/bootstrap/install-kernel.sh
+++ home/vlt-adm/bootstrap/install-kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 KERNEL="vulture-hardenedbsd"
+KERNEL_TOOLS="vulture-hardenedbsd-tools"
 
 if [ "$(/usr/bin/id -u)" != "0" ]; then
    /bin/echo "This script must be run as root" 1>&2
@@ -8,11 +9,11 @@ if [ "$(/usr/bin/id -u)" != "0" ]; then
 fi
 
 bsd_version=$(/usr/bin/uname -r | /usr/bin/cut -d '-' -f 1)
+
 base_url="https://download.vultureproject.org/v4/${bsd_version}/kernel/${KERNEL}"
 /bin/rm -f /var/tmp/${KERNEL}.txz
 /usr/local/bin/wget "$base_url-latest.txz" -O /var/tmp/${KERNEL}.txz
 /usr/local/bin/wget "$base_url-latest.sha256sum" -O /var/tmp/${KERNEL}.sha256sum
-
 /bin/echo -n "Verifying SHASUM for ${KERNEL}.txz... "
 /sbin/sha256 -c /var/tmp/${KERNEL}.sha256sum > /dev/null
 if [ "$?" == "0" ]; then
@@ -22,5 +23,21 @@ else
 	exit
 fi
 
+base_url="https://download.vultureproject.org/v4/${bsd_version}/kernel/${KERNEL_TOOLS}"
+/bin/rm -f /var/tmp/${KERNEL_TOOLS}.txz
+/usr/local/bin/wget "$base_url-latest.txz" -O /var/tmp/${KERNEL_TOOLS}.txz
+/usr/local/bin/wget "$base_url-latest.sha256sum" -O /var/tmp/${KERNEL_TOOLS}.sha256sum
+/bin/echo -n "Verifying SHASUM for ${KERNEL_TOOLS}.txz... "
+/sbin/sha256 -c /var/tmp/${KERNEL_TOOLS}.sha256sum > /dev/null
+if [ "$?" == "0" ]; then
+    /bin/echo "Ok!"
+else
+    /bin/echo "Bad shasum for ${KERNEL_TOOLS}.txz"
+	exit
+fi
+
 /usr/bin/tar xvf /var/tmp/${KERNEL}.txz -C /boot/
+/usr/bin/tar xvf /var/tmp/${KERNEL_TOOLS}.txz -C /
+
 /bin/rm -f /var/tmp/${KERNEL}.txz
+/bin/rm -f /var/tmp/${KERNEL_TOOLS}.txz
