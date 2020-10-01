6204b597bbc1c0aabb8ab0c58f5e47c19f6ce468
Jérémie <jeremie.jourdin@gmail.com>
2020-10-01 20:38:15 +0200

    Revert "Add support for masquerading IP address. This allow to have outgoing trafic masqueraded behind a custom IP address"
    This reverts commit 65f2da0a04955e7cfc2244a9b561d92afa4c734e.

--- usr/local/bin/pfctl-get_management.sh
+++ usr/local/bin/pfctl-get_management.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script automatically create an IP management address
+#This script automatically create an IP management address
 /usr/bin/touch /usr/local/etc/management.ip
 /usr/sbin/chown root:wheel /usr/local/etc/management.ip
 /bin/chmod 644 /usr/local/etc/management.ip
@@ -8,10 +8,3 @@ ip="$(/sbin/ifconfig | /usr/bin/grep inet | /usr/bin/grep -v '127.0.0.1' \
       | /usr/bin/grep -v ' ::1 ' | /usr/bin/grep -v 'fd00::20' | /usr/bin/grep -v 'fe80:' \
       | /usr/bin/awk '{print $2}' | /usr/bin/awk -vRS="" -vOFS=' ' '$1=$1')"
 /bin/echo "$ip" | /usr/bin/sed -e 's/ .*//' > /usr/local/etc/management.ip
-
-# Also create the masquerading.ip if file does not exists
-if ! [ -f /usr/local/etc/masquerading.ip ] ; then
-    cp -f /usr/local/etc/management.ip /usr/local/etc/masquerading.ip
-    /usr/sbin/chown root:wheel /usr/local/etc/masquerading.ip
-    /bin/chmod 644 /usr/local/etc/masquerading.ip
-fi
