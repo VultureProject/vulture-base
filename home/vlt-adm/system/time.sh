3549082d1aa75aa6c9615a417769523322d8b4a1
Kevin Guillemot <kevin.guillemot@advens.fr>
2021-06-02 17:56:31 +0200

    ADMIN.SH :: Fix : NTP configuration wasn't reused in dialog

--- home/vlt-adm/system/time.sh
+++ home/vlt-adm/system/time.sh
@@ -4,7 +4,12 @@
 tmp_file="/var/tmp/dialog$$"
 /bin/rm -f "$tmp_file"
 
-if /usr/bin/dialog --title "Vulture NTP setting" --inputbox "Enter NTP server address" 8 60 "0.freebsd.pool.ntp.org" 2> "$tmp_file"; then
+current_ntp="0.freebsd.pool.ntp.org"
+if [ -f /etc/rc.conf.ntp ] ; then
+    current_ntp="$(/bin/cat /etc/rc.conf.ntp)"
+fi
+
+if /usr/bin/dialog --title "Vulture NTP setting" --inputbox "Enter NTP server address" 8 60 "$current_ntp" 2> "$tmp_file"; then
     if [ -f "$tmp_file" ]; then
         ntp=$(/bin/cat "$tmp_file")
         /usr/local/bin/sudo /home/vlt-adm/system/write_ntp.sh "${ntp}"
