263e0bf2e678753095c32facc14ba1f38d627388
frikilax <bertintheo@orange.fr>
2022-11-08 15:02:43 +0100

    PROXY.SH::ADDED:: Set proxy settings in pkg.conf to use proxy transparently

--- home/vlt-adm/system/proxy.sh
+++ home/vlt-adm/system/proxy.sh
@@ -11,14 +11,23 @@ export http_proxy=http://${proxy}
 export https_proxy=http://${proxy}
 export ftp_proxy=http://${proxy}
 
-if [ "${proxy}" != "" ]; then
+if [ -n "${proxy}" ]; then
     /bin/echo "http_proxy=http://${proxy}" > /etc/rc.conf.proxy
     /bin/echo "https_proxy=http://${proxy}" >> /etc/rc.conf.proxy
     /bin/echo "ftp_proxy=http://${proxy}" >> /etc/rc.conf.proxy
     # Copy proxy conf to jails
     for dir in /zroot/*/etc/ ; do /bin/cp /etc/rc.conf.proxy "$dir" ; done
+    # update pkg.conf file to force pkg to use proxy
+    if /usr/bin/grep -q "^PKG_ENV" /usr/local/etc/pkg.conf; then
+        sed -i '' "s+^PKG_ENV.*+PKG_ENV {http_proxy: http://${proxy}, https_proxy: http://${proxy}}+g" /usr/local/etc/pkg.conf
+    else
+        /bin/echo "" >> /usr/local/etc/pkg.conf
+        /bin/echo "PKG_ENV {http_proxy: http://${proxy},https_proxy: http://${proxy}}+g" >> /usr/local/etc/pkg.conf
+    fi
 else
     /bin/rm /etc/rc.conf.proxy
     # Remove proxy from jails
     /bin/rm /zroot/*/etc/rc.conf.proxy
+    # update pkg.conf file to force pkg to use proxy
+    sed -i '' 's+^PKG_ENV.*+PKG_ENV {}+g' /usr/local/etc/pkg.conf
 fi
