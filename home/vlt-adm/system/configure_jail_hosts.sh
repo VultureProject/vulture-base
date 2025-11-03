a8bb6bf7e248fac4b39c7a87138bee1fc20893d4
Fabien Amelinck <fabien.amelinck@advens.fr>
2025-11-03 09:45:40 +0100

    change(resolv): fix ips

--- home/vlt-adm/system/configure_jail_hosts.sh
+++ home/vlt-adm/system/configure_jail_hosts.sh
@@ -15,17 +15,41 @@ JAIL_NAME="$1"
 TARGET="/zroot/${JAIL_NAME}"
 
 # Configure /etc/hosts of jail
-/bin/echo "127.0.0.1 localhost" > ${TARGET}/etc/hosts
-/bin/echo "::1 localhost" >> ${TARGET}/etc/hosts
+/bin/echo "::1 localhost" > ${TARGET}/etc/hosts
+/bin/echo "127.0.0.1 localhost" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::202 mongodb" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.2 mongodb" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::203 redis" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.3 redis" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::204 rsyslog" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.4 rsyslog" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::205 haproxy" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.5 haproxy" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.6 apache" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::206 apache" >> ${TARGET}/etc/hosts
+/bin/echo "127.0.0.7 portal" >> ${TARGET}/etc/hosts
+/bin/echo "fd00::207 portal" >> ${TARGET}/etc/hosts
 
-i=2
-for jail in mongodb redis rsyslog haproxy apache portal; do
-    /bin/echo "127.0.0.$i $jail" >> ${TARGET}/etc/hosts
-    /bin/echo "fd00::20$i $jail" >> ${TARGET}/etc/hosts
-
-    # Host's dnsmasq resolver is used by jails -> local loopback of the jail
-    if [ "$jail" = "$JAIL_NAME" ]; then
-        echo "nameserver 127.0.0.$i" > ${TARGET}/etc/resolv.conf
-    fi
-    i=$((i + 1))
-done
+# Host's dnsmasq resolver is used by jails -> local loopback of the jail
+case "$JAIL_NAME" in
+    mongodb)
+        /bin/echo "nameserver 127.0.0.2" > ${TARGET}/etc/resolv.conf
+        ;;
+    redis)
+        /bin/echo "nameserver 127.0.0.3" > ${TARGET}/etc/resolv.conf
+        ;;
+    rsyslog)
+        /bin/echo "nameserver 127.0.0.4" > ${TARGET}/etc/resolv.conf
+        ;;
+    haproxy)
+        /bin/echo "nameserver 127.0.0.5" > ${TARGET}/etc/resolv.conf
+        ;;
+    apache)
+        /bin/echo "nameserver 127.0.0.6" > ${TARGET}/etc/resolv.conf
+        ;;
+    portal)
+        /bin/echo "nameserver 127.0.0.7" > ${TARGET}/etc/resolv.conf
+        ;;
+    *)
+        ;;
+esac
